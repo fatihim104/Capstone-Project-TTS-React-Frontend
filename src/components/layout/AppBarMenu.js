e9619e6dcd377d335026497a48ad3f74bf6d6da8
yildirim038 <aosmanyildirim@gmail.com>
2021-07-23 17:07:25 +0200

    d

--- src/components/layout/AppBarMenu.js
+++ src/components/layout/AppBarMenu.js
@@ -3,13 +3,15 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+
 import IconButton from '@material-ui/core/IconButton';
 import Avatar from '@material-ui/core/Avatar';
-
+import LoginButton from '../login/LoginButton'
+import LogoutButton from '../login/LogoutButton'
+import { useAuth0 } from "@auth0/auth0-react";
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,    
+    flexGrow: 1,
   },
   menuButton: {
     marginRight: theme.spacing(1),
@@ -17,28 +19,53 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
- 
+
 }
 ));
 
 const AppBarMenü = () => {
   const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar >
-          <IconButton edge="start" className={classes.menuButton}  aria-label="menu">
-            <Avatar alt="-name-" src="" />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Welcome (..name..)
-          </Typography>
-          <Button color="inherit">Logout</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+  const { user } = useAuth0();
+  if (user === undefined) {
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar >
+            <IconButton edge="start" className={classes.menuButton} aria-label="menu">
+              < Avatar alt="-name-" src="https://goktemtemizlik.com/wp-content/uploads/2018/11/esenyurt-temizlik.png" />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Welcome
+            </Typography>
+            <LoginButton />
+            <LogoutButton />
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+  else {
+    const { name, picture } = user;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar >
+            <IconButton edge="start" className={classes.menuButton} aria-label="menu">
+              <img
+                src={picture}
+                alt="Profile"
+                className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
+              />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Welcome {<p>{name}</p>}
+            </Typography>
+            <LoginButton />
+            <LogoutButton />
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
-
 export default AppBarMenü;
